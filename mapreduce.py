7266af8c27a9e6459dc6ee9ac31ebe44151906a3
baburajv <baburajv@gmail.com>
2018-08-09 13:06:21 +0530

    MONIT-9683 add error handling in mapreduce.py script

--- mapreduce.py
+++ mapreduce.py
@@ -2,23 +2,38 @@ import argparse
 import urllib2
 import json
 import datetime
+import sys
 
 def doMain():
     url = '%s/ws/v1/cluster/apps/' % args.server
-    response = urllib2.urlopen(url)
-    data = json.loads(response.read())
-    if "apps" in data:
-        apps = []
-        for app in data["apps"]["app"]:
-            if valid(int(app["finishedTime"]/1000.0)) :
-                apps.append(app)
-        print(json.dumps(apps, ensure_ascii=False))
+    try:
+        response = urllib2.urlopen(url)
+    except urllib2.HTTPError as e :
+       handle_error( "HTTPError " + str(e))
+    except urllib2.URLError as e:
+        handle_error( "URLError " + str(e))
+    except httplib.HTTPException as e:
+        handle_error( "HTTPException " + str(e))
+    except Exception as e :
+        handle_error( "Generic Exception " + str(e))
+    else:
+        data = json.loads(response.read())
+        if "apps" in data:
+            apps = []
+            for app in data["apps"]["app"]:
+                if valid(int(app["finishedTime"]/1000.0)) :
+                    apps.append(app)
+            print(json.dumps(apps, ensure_ascii=False))
 
 def valid(finishedTime):
     seconds = (datetime.datetime.now() - datetime.datetime.fromtimestamp(finishedTime)).seconds
     if (finishedTime != 0) and (seconds >= 300):
         return False
     return True
+
+def handle_error(error_message):
+    sys.stderr.write("ERROR:|MapReduce| " + error_message)
+    sys.exit(1)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
